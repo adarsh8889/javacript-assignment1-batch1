f5650e2ce4bac54fe21bc8f639efee66d60bf9bc
adarsh8889 <88422939+adarsh8889@users.noreply.github.com>
2022-01-29 18:09:36 +0530

    Add files via upload

---
+++ positive negative zero.js
@@ -0,0 +1,10 @@
+let num = 65
+if (num > 0) {
+    console.log("The num is positive");
+}
+else if (num == 0) {
+  console.log("The num is 0");
+}
+else {
+     console.log("The num is negative");
+}
